0e91f34b77a9341cf574faad4ce40dcdba93f9ea
Ameya Shinde <ameyashinde123@outlook.com>
2022-08-05 02:58:37 +0530

    Added page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,24 @@ import { AddContactComponent } from './components/add-contact/add-contact.compon
 import { EditContactComponent } from './components/edit-contact/edit-contact.component';
 import { ViewContactComponent } from './components/view-contact/view-contact.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    ContactManagerComponent,
-    AddContactComponent,
-    EditContactComponent,
-    ViewContactComponent,
-    SpinnerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        ContactManagerComponent,
+        AddContactComponent,
+        EditContactComponent,
+        ViewContactComponent,
+        SpinnerComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
